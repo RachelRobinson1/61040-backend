2e4ae7b162f6632e65457ed964d19e3e8351adec
Rachel Robinson <rachel73@mit.edu>
2023-10-03 23:59:36 -0400

    assignment 4

--- server/app.ts
+++ server/app.ts
@@ -1,5 +1,9 @@
+import ChatConcept from "./concepts/chat";
+import CollectionConcept from "./concepts/collection";
 import FriendConcept from "./concepts/friend";
 import PostConcept from "./concepts/post";
+import Q_AConcept from "./concepts/q&a";
+import ReactionConcept from "./concepts/reaction";
 import UserConcept from "./concepts/user";
 import WebSessionConcept from "./concepts/websession";
 
@@ -8,3 +12,7 @@ export const WebSession = new WebSessionConcept();
 export const User = new UserConcept();
 export const Post = new PostConcept();
 export const Friend = new FriendConcept();
+export const Collection = new CollectionConcept();
+export const Chat = new ChatConcept();
+export const Reaction = new ReactionConcept();
+export const Q_A = new Q_AConcept();
